1d5b9dfad3ec910671e433cfeb5d5ac6e2de3ac8
ZhangShuang <vindinser@gmail.com>
2023-12-09 17:19:06 +0800

    完成用户登录功能

--- src/views/login/Login.js
+++ src/views/login/Login.js
@@ -1,10 +1,177 @@
 // 登录
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {Button, Checkbox, Form, Input, message} from "antd";
+import {UserOutlined, LockOutlined} from "@ant-design/icons";
+import "./Login.scss";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim";
+import {login} from "../../apis/urls";
 
-const Login = () => {
+const Login = (props) => {
+  const [ init, setInit ] = useState(false);
+  // this should be run only once per application lifetime
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+      // starting from v2 you can add only the features you need reducing the bundle size
+      //await loadAll(engine);
+      //await loadFull(engine);
+      await loadSlim(engine);
+      //await loadBasic(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+
+  /**
+   * 登录表单 登录按钮点击事件
+   * @param param 登录表单所填项
+   */
+  const onFinish = (param) => {
+    login(param).then(res => {
+      if(res.length === 0) return message.error("用户名密码不匹配");
+      localStorage.setItem("token", JSON.stringify(res[0]));
+      props.history.push("/");
+    }).catch(err => {
+      console.error(err);
+    });
+  }
+
   return (
-    <div>
-      login
+    <div className="login-box">
+      { init && <Particles
+        id="tsparticles"
+        particlesLoaded={particlesLoaded}
+        options={{
+          background: {
+            color: {
+              value: "rgb(35, 39, 65)",
+            },
+          },
+          fpsLimit: 120,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              onHover: {
+                enable: true,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            modes: {
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#ffffff",
+            },
+            links: {
+              color: "#ffffff",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outModes: {
+                default: "bounce",
+              },
+              random: false,
+              speed: 6,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 800,
+              },
+              value: 80,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              value: { min: 1, max: 5 },
+            },
+          },
+          detectRetina: true,
+        }}
+      />
+      }
+      <div className="login-box_form">
+        <div className="login-box_title">全球新闻发布系统</div>
+        <Form
+          name="normal_login"
+          className="login-form"
+          onFinish={onFinish}
+        >
+          <Form.Item
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your Username!',
+              },
+            ]}
+          >
+            <Input prefix={<UserOutlined className="site-form-item-icon"/>} placeholder="Username"/>
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your Password!',
+              },
+            ]}
+          >
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon"/>}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Form.Item name="remember" valuePropName="checked" noStyle>
+              <Checkbox>
+                <a className="login-form-forgot" href="">
+                  Remember me
+                </a>
+              </Checkbox>
+            </Form.Item>
+            <a className="login-form-forgot" href="">
+            Forgot password
+            </a>
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit" className="login-form-button">
+              登录
+            </Button>
+            Or <a href="">register now!</a>
+          </Form.Item>
+        </Form>
+      </div>
     </div>
   )
 }
